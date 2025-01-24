102291d1cc3e53c7ff92148e3d22c7ea435d183c
Rutuja1923 <rutujajirage19@gmail.com>
2025-01-24 16:21:44 +0530

    server to render ejs files

---
+++ EJS-Templating/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
+});
