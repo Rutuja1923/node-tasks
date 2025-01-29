905b1e5bee5b89c86e2ebc7130a47c9410d863a4
Rutuja1923 <rutujajirage19@gmail.com>
2025-01-29 19:59:48 +0530

    User Schema

--- DB_demo/restAPIwithDB.js
+++ DB_demo/restAPIwithDB.js
@@ -14,3 +14,33 @@ mongoose
     .catch( (err) => {
         console.error(`Mongo Error: ${err.message}`);
     });
+
+//schema
+const userSchema = new Schema(
+    {
+        first_name: {
+            type: String,
+            required: true
+        },
+        last_name: {
+            type: String,
+            required: false
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        gender: {
+            type: String,
+            required: true
+        },
+        job_title: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
