507248fed8fc80db9ebaec6dcc2cb8a50436419e
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-25 21:09:43 +0530

    Route redistery, exoress instance, global middleware

--- file-upload/index.js
+++ file-upload/index.js
@@ -1,36 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const path = require('path');
 const express = require('express');
 const multer = require('multer');
+const cookieParser = require("cookie-parser");
+
+const {connectMongoDB} = require('./connection');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 3000;
 
-const storage = multer.diskStorage( {
-    destination: function (req, file, cb) { 
-        return cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-} );
-
-const upload = multer( {storage} );
+const url = process.env.MONGO_URI;
+connectMongoDB(url);
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
 app.use(express.urlencoded( { extended: false } ));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
+
 
 app.get('/', (req, res) => {
     return res.render('homepage');
 });
 
-app.post("/upload", upload.single("profileImage"), (req, res) => {
-    console.log(req.body);
-    console.log(req.file);
-
-    return res.redirect('/');
-});
+app.use("/", authRoutes);
+app.use("/", userRoutes);
 
 app.listen(PORT , () => {
     console.log("Server is listening on PORT : 3000");
